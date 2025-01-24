e9478f503ebeac32a21f0e8bc097d4698bce65a8
Yakubu Alhassan <yaqoubdramani@gmail.com>
2025-01-24 07:42:30 +0000

    Update rootlayout with toast config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,5 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { View, Text } from 'react-native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -6,6 +7,7 @@ import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
+import Toast from 'react-native-toast-message';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -34,6 +36,14 @@ export default function RootLayout() {
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
+      <Toast config={{
+        success: ({ text1, text2 }) => (
+          <View style={{ padding: 15, backgroundColor: 'green' }}>
+            <Text style={{ color: 'white' }}>{text1}</Text>
+            <Text style={{ color: 'white' }}>{text2}</Text>
+          </View>
+        ),
+      }} />
       <StatusBar style="auto" />
     </ThemeProvider>
   );
